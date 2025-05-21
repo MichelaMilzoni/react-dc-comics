8c568728b1d3e0e6be805fa5341ebaadd06dcf4a
MichelaMilzoni <michelamilzoni0@gmail.com>
2025-05-21 16:53:45 +0200

    add type="module"  index.html

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,14 @@
-// import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import '../src/styles/index.css'
+import './styles/index.css'
 
-createRoot(document.getElementById("root")).render(
-  <App />
-);
+// createRoot(document.getElementById("root")).render(
+//   <App />
+// );
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
